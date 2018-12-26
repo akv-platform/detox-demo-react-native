5105da3d35ff34101d05515b9ce5d2a7c1a18fc3
Denis Panov <40772628+denis-panov@users.noreply.github.com>
2018-12-26 15:11:41 +0300

    Clean up emulator launch script

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -9,8 +9,6 @@ then
   cd ..
   export PATH=$PATH:./simutils/build/Build/Products/Release
 else
-  spctl kext-consent list
-  echo "------------------------------------LOG---------------------------------------------------------------"
   echo "Creating an Android emulator..."
   cd $ANDROID_HOME/tools/bin
   echo "y" | ./sdkmanager "system-images;android-19;default;arm64-v8a"
@@ -26,32 +24,26 @@ else
   echo "LOG: emulator -list-avds"
   $ANDROID_HOME/emulator/emulator -list-avds
 
-  echo "Starting the Android emulator..."
-  
-  cd $ANDROID_HOME/emulator
-  cd $(dirname $(which emulator))
-  nohup emulator -avd Nexus_5X_API_26 -no-snapshot > /dev/null 2>&1 &
-  sleep 5
- 
-  echo "Wait for the Android emulator to run..."
-  $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
-  echo "Ensure emulator run..."
-  $ANDROID_HOME/platform-tools/adb devices
+  echo "LOG: emulator -list-avds"
+  $ANDROID_HOME/emulator/emulator -list-avds
 
-  echo "----------------------------"
-  echo "LOG: nohup.out"
-  cat nohup.out
-  echo "LOG: spctl kext-consent list"
-  spctl kext-consent list
-  echo "----------------------------"
+  echo "Starting the Android emulator..."
+  $ANDROID_HOME/emulator/emulator64-arm -avd Nexus_5X_API_26 -no-window -gpu off &
+  sleep 5
+
+  echo "=================Waiting device:================="
+  EMU_BOOTED='unknown'
+  while [[ ${EMU_BOOTED} != *"1"* ]]; do
+      echo "Waiting emulator to start..."
+      sleep 5
+      EMU_BOOTED=`adb shell 'getprop sys.boot_completed | tr -d "\r"'`
+      echo "$EMU_BOOTED"
+  done
+  echo "Android Emulator started...."
+  echo "=================================================="
 
   echo "Ensure emulator run:"
   $ANDROID_HOME/platform-tools/adb devices
-
-  echo "LOG : adb shell ls"
-  $ANDROID_HOME/platform-tools/adb wait-for-device shell ls
-  echo "---------POWER-------------------"
-  $ANDROID_HOME/platform-tools/adb shell service list | grep power
   cd $APPCENTER_SOURCE_DIRECTORY
 fi
 
