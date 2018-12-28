959a992232e0d9a92af97e78ad654436bc6609bd
Sergey Dolin <v-sedoli@microsoft.com>
2018-12-29 01:26:31 +0500

    Apply post build worked in public env

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,62 +1,64 @@
-if [ -z "$APPCENTER_ANDROID_VARIANT"];
-then 
-  echo "Installing applesimutils..."
-  mkdir simutils
-  cd simutils
-  curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
-  tar xzvf applesimutils.tar.gz
-  sh buildForBrew.sh .
-  cd ..
-  export PATH=$PATH:./simutils/build/Build/Products/Release
-else
-  spctl kext-consent list
-  echo "------------------------------------LOG---------------------------------------------------------------"
-  echo "Creating an Android emulator..."
-  cd $ANDROID_HOME/tools/bin
-  echo "y" | ./sdkmanager "system-images;android-25;google_apis;arm64-v8a"
-  for i in {1..4};do echo "y"; done | ./sdkmanager --licenses
-  touch ~/.android/repositories.cfg
+set +x
+WORK_DIR=`pwd`
 
-  echo "no" | ./avdmanager create avd --force -n Nexus_5X_API_26 -k "system-images;android-25;google_apis;arm64-v8a" 
+echo "Creating an Android emulator..."
+cd $ANDROID_HOME/tools/bin
+echo "y" | ./sdkmanager "system-images;android-26;google_apis;x86"
+for i in {1..4};do echo "y"; done | ./sdkmanager --licenses
+touch ~/.android/repositories.cfg
 
-  echo "LOG: emulator -list-avds"
-  $ANDROID_HOME/emulator/emulator -list-avds
+echo "no" | ./avdmanager create avd --force -n Nexus_5X_API_26 -k "system-images;android-26;google_apis;x86"
 
-  echo "Starting the Android emulator..."
-  
-  cd $ANDROID_HOME/emulator
-  cd $(dirname $(which emulator))
-  nohup emulator -avd Nexus_5X_API_26 -no-snapshot > /dev/null 2>&1 &
-  sleep 5
- 
-  echo "Wait for the Android emulator to run..."
-  $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do sleep 1; done; input keyevent 82'
-  echo "Ensure emulator run..."
-  $ANDROID_HOME/platform-tools/adb devices
+echo "Starting the Android emulator..."
 
-  echo "----------------------------"
-  echo "LOG: nohup.out"
-  cat nohup.out
-  echo "LOG: spctl kext-consent list"
-  spctl kext-consent list
-  echo "----------------------------"
+cd $ANDROID_HOME/emulator
+nohup emulator -avd Nexus_5X_API_26 -no-snapshot > /dev/null 2>&1 &
 
-  echo "Ensure emulator run:"
-  $ANDROID_HOME/platform-tools/adb devices
+echo "Wait for the Android emulator process started..."
+i=1
+while ! $ANDROID_HOME/platform-tools/adb devices |grep 'emulator-';do
+   if [ $i -eq 10 ];then
+    echo "No emulator process started in 10 secs"
+    exit 1
+  fi
+  echo "...wait process... $i sec"
+  sleep 1
+  i=$((i+1))
+done
 
-  echo "LOG : adb shell ls"
-  $ANDROID_HOME/platform-tools/adb wait-for-device shell ls
+echo "Wait for the Android emulator device ready..."
+i=1
+while ! $ANDROID_HOME/platform-tools/adb devices |grep '^emulator-.*device$';do
+  if [ $i -eq 60 ];then
+    echo "No emulator attached in 1 min"
+    $ANDROID_HOME/platform-tools/adb devices
+    exit 2
+  fi
+  if ! $ANDROID_HOME/platform-tools/adb devices |grep -q 'emulator-';then
+    echo "Emulator has gone"
+    $ANDROID_HOME/platform-tools/adb devices
+    exit 3
+  fi
+  echo "...wait device... $i sec"
+  sleep 1
+  i=$((i+1))
+done
 
-  cd $APPCENTER_SOURCE_DIRECTORY
+echo "Wait for the Android emulator to boot..."
 
-	echo "Existing config.ini"
+$ANDROID_HOME/platform-tools/adb shell 'i=1;while [[ -z $(getprop sys.boot_completed | tr -d '\r') ]]; do echo "...wait boot... $i sec";sleep 1; i=$((i+1));done; input keyevent 82'
 
-	test -f /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini && cat /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini || echo "UNEXPECTED: No config.ini"
+echo "adb devices..."
+$ANDROID_HOME/platform-tools/adb devices
 
-  echo "Modifying config..."
-  echo "hw.lcd.width=1080" >> /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini
-  echo "hw.lcd.height=1920" >> /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini
-fi
+echo "Existing config.ini"
+
+test -f /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini && cat /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini || echo "UNEXPECTED: No config.ini"
+
+echo "Modifying config..."
+echo "hw.lcd.width=1080" >> /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini
+echo "hw.lcd.height=1920" >> /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini
+cat /Users/vsts/.android/avd/Nexus_5X_API_26.avd/config.ini
 
 echo "Installing NVM..."
 brew install nvm
@@ -73,23 +75,16 @@ node --version
 echo "Installing detox cli..."
 npm install -g detox-cli
 
+cd $WORK_DIR/examples/demo-react-native
+
 echo "Installing dependencies for detox tests..."
 npm install
 
+echo "Building the project..."
+./node_modules/.bin/detox build --configuration android.emu.release
 
-if [ -z "$APPCENTER_ANDROID_VARIANT"];
-then 
-  echo "Building the project..." 
-  ./node_modules/.bin/detox build --configuration ios.sim.release 
-  echo "Executing tests..." 
-  ./node_modules/.bin/detox test --configuration ios.sim.release --cleanup
-else
-  echo "Building the project..." 
-  ./node_modules/.bin/detox build --configuration android.emu.debug
+echo "Install app..."
+$ANDROID_HOME/platform-tools/adb install android/app/build/outputs/apk/release/app-debug.apk
 
-  echo "Install app..." 
-  $ANDROID_HOME/platform-tools/adb install android/app/build/outputs/apk/debug/app-debug.apk
-
-  echo "Executing tests..." 
-  ./node_modules/.bin/detox test --loglevel verbose --configuration android.emu.debug --cleanup --debug-synchronization 1000
-fi
+echo "Executing tests..."
+./node_modules/.bin/detox test --loglevel verbose --configuration android.emu.release --cleanup --debug-synchronization 1000
